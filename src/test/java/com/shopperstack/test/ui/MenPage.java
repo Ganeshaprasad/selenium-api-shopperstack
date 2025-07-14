182857b4d7ad131fdca5b1d4254af2254f2787ed
Ganeshaprasad <ganesha.prasad1996@gmail.com>
2025-07-14 12:50:49 +0530

    added listner implemntation to take screen shot

--- src/test/java/com/shopperstack/test/ui/MenPage.java
+++ src/test/java/com/shopperstack/test/ui/MenPage.java
@@ -1,12 +1,13 @@
 package com.shopperstack.test.ui;
 
 import com.shopperstack.base.BaseLoggedInTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MenPage extends BaseLoggedInTest {
     @Test
     public void dummyTest()
     {
-        //System.out.println("test pass");
+        Assert.fail();
     }
 }
